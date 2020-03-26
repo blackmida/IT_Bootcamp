ef4393a433e2942d92d6cbfd1c8c8e0cb4ab56fb
blackmida <blackmida@gmail.com>
2020-03-26 10:53:12 +0100

    Stilizacija i poboljsanja ChatProjecta

---
+++ Predavanja/30Cas - ProjectChat/public/app.js
@@ -0,0 +1,132 @@
+//Moduli
+import {Chatroom} from "./chat.js"
+import {ChatUI} from "./ui.js"
+
+//DOM elementi
+let chatList = document.querySelector('#ulMessages');
+
+let formMessage = document.querySelector('#formMessage');
+let inputMessage = document.querySelector('#inputMessage');
+
+let formUsername = document.querySelector('#formUsername');
+let inputUsername = document.querySelector('#inputUsername');
+
+let rooms = document.querySelector('nav');
+let updatedUsername = document.querySelector('#divUpdatedUsername');
+
+let buttons = document.querySelectorAll('button');
+
+//Provera username u lokalnoj memoriji
+let checkUsername = () => {
+    if(localStorage.usernameLS){
+        return localStorage.usernameLS;
+    } else {
+        return "anonymous";
+    }
+}
+
+//Kreiranje instanci klasa
+let chatroom = new Chatroom('js', checkUsername());
+let chatui = new ChatUI(chatList);
+
+//Provera prilikom učitanja stranice
+//Koja soba je bila poslednja posećena, nju učitati 
+let checkRoom = () => {
+    if(localStorage.roomLS){
+        return localStorage.roomLS;
+    } else {
+        return "general";
+    }
+}
+chatroom.updateRoom(checkRoom());
+
+//Poslednjoj posećenoj sobi dodati klasu koja će da oboji to dugme
+buttons.forEach( b => {
+    if(b.id == checkRoom()){
+        b.classList.add('btn-selected');
+    }
+});
+
+//Ispisi poruke
+chatroom.getChets( data => {
+    //console.log(data); //u konzoli
+    chatui.templateLI(data);
+});
+
+//Kada je submit dugme Send pošalji poruku
+formMessage.addEventListener('submit' , e => {
+    e.preventDefault();
+    let message = inputMessage.value;
+    chatroom.addChat(message)
+        .then( () => formMessage.reset())
+        .catch( error => console.log(error));
+        
+    //Izbrisati sve poruke sa stanice
+    chatui.clear();
+
+    //Učitati poruke za promenjenu sobu
+    chatroom.getChets( data => {
+        //console.log(data); //u konzoli
+        chatui.templateLI(data);
+    });
+
+});
+
+//Kada je submit dugme Update izmeni korisničko ime
+formUsername.addEventListener('submit' , e => {
+    e.preventDefault();
+    let patternUsername = /^[\S]*[\S]$/;
+    let newUsername = inputUsername.value;
+    if(patternUsername.test(newUsername)){
+    chatroom.updateUsername(newUsername);
+    formUsername.reset();
+
+    //Osvežiti čet prilikom promene korisničkog imena tako što ga izbrišemo i učitamo ponovo
+    chatui.clear();
+    chatroom.getChets(data => {
+        chatui.templateLI(data);
+    });
+
+    //Kada je Update ime prikazati poruku u trajanju od 3s
+    updatedUsername.innerHTML = `Your username was updated to 
+    <span id="spanNewUsername">${newUsername}</span>`;
+    setTimeout(()=>{
+        updatedUsername.innerHTML = ``;
+    } , 3000);
+    }
+    else{
+        alert('Pogresan username!')
+    }
+});
+
+//Promena soba
+rooms.addEventListener('click' , e => {
+    if(e.target.tagName == "BUTTON"){
+        //Kada je kliknuto na dugme, dodeliti mu klasu da je baš ono selektovano
+        
+        buttons.forEach( b => {
+            //"Očistiti" dugme od btn-selected klase
+            if(b.classList.contains('btn-selected')){
+                b.classList.remove('btn-selected');
+            }            
+        });
+        let btnId = e.target.getAttribute('id');
+        let btn = document.getElementById(btnId);
+
+        btn.classList.add('btn-selected');
+
+        //Izbrisati sve poruke sa stanice
+        chatui.clear();
+
+        //Promeniti sobu
+        let newRoom = e.target.getAttribute('id');
+        chatroom.updateRoom(newRoom);
+        localStorage.setItem('roomLS' , newRoom);
+
+        //Učitati poruke za promenjenu sobu
+        chatroom.getChets( data => {
+            //console.log(data); //u konzoli
+            chatui.templateLI(data);
+        });
+    }
+});
---
+++ Predavanja/30Cas - ProjectChat/public/chat.js
@@ -0,0 +1,110 @@
+export class Chatroom{
+
+    // Konstruktor
+    constructor(r, u){
+        this.room = r;
+        this.username = u;
+        this.chats = db.collection('chats');
+        this.unsub;
+    }
+
+    // Seteri
+    set username(u){
+        this._username = u;
+    }
+    set room(r){
+        this._room = r;
+    }
+
+    // Geteri
+    get username(){
+        return this._username;
+    }
+    get room(){
+        return this._room;
+    }
+
+    async addChat(mess){
+
+        //Dohvatanje tekućeg datuma koji je potreban za timestamp
+        let date = new Date();
+
+        // Kreiranje objekta/dokumenta koji prosleđujemo bazi podataka
+        let docChat = {
+            message: mess,
+            username: this.username,
+            room: this.room,
+            created_at: firebase.firestore.Timestamp.fromDate(date)
+        };
+
+        //Da sačuvamo dokument u bazi
+        let response = await this.chats.add(docChat); 
+        return response;
+    }
+
+    getChets(callback){
+        this.unsub = this.chats
+                    .where('room','==',this.room)
+                    .orderBy('created_at')
+                    .onSnapshot( snapshot => {
+                        snapshot.docChanges().forEach( change => {
+                            //Ako je poruka dodata u bazu onda raditi update
+                            if( change.type === 'added' ){
+                                //uradi update
+                                callback(change.doc.data());
+                            }
+                        });
+                    });
+    }
+
+    updateUsername(uu){
+        //Suštinski menja samo vrednost lokalne promenljive, ne menja vrednost username u bazi podataka
+        this.username = uu;
+        localStorage.setItem('usernameLS', uu);
+    }
+
+    updateRoom(ur){
+        this.room = ur;
+        //console.log("Updated room");
+        if(this.unsub){
+            this.unsub();
+        }
+    }
+
+}
+
+//Nova instanca klase (Novi objekat)
+//let chatroom = new Chatroom('js', 'PeraPeric');
+
+//console.log(chatroom);
+/*
+chatroom.addChat('Zdravo!')
+    .then( () => {console.log('Čet je dodat!');})
+    .catch( err => {console.log(err);});
+*/
+
+/*
+chatroom.getChets( data => {
+    console.log(data);
+});
+
+chatroom.updateRoom("general");
+
+chatroom.getChets( data => {
+                    console.log(data);
+                });
+
+chatroom.updateRoom('js');
+
+chatroom.getChets( data => {console.log(data);} );
+*/
+/*
+setTimeout( () => {
+    chatroom.updateRoom('js');
+    chatroom.updateUsername('Tina');
+    chatroom.getChets( data => {
+        console.log(data);
+    });
+    chatroom.addChat("Kako si Milice?");
+}, 3000);
+*/
